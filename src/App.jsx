6b5926b759d32927af76501f1bdc1f3d32642f88
Santiago David <santiagodaviduno@hotmail.com>
2022-08-06 19:15:26 -0400

    si

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       clearInterval(timerId);
       break;
     case "changerButton":
-      colorChanger();
+      colorChanger().then();
       setOption(null);
       break;
     case "sinButton":
